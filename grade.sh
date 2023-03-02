76bf2c21217a3dcf745832f19e8f3467c29ead5d
sgaywalaUCSD <sgaywala@ucsd.edu>
2023-03-02 13:34:02 -0800

    fixed grade.sh

--- grade.sh
+++ grade.sh
@@ -25,20 +25,42 @@ else
     exit 2
 fi
 
-javac -cp ".;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar" *.java
-java -cp ".;../lib/junit-4.13.2.jar;../lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples
+javac -cp $CPATH *.java
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
 
 
-java -cp $CPATH org.junit.runner.JUnitCore student-submission/*.java > output.txt
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > output.txt
 
-grep "Tests run" output.txt > grepResults.txt
+tail -n 2 output.txt | head -n 1 > grepResults.txt
 
-TESTSRUN=` cut -d ',' -f 1 grepResults.txt `
+if grep -q "OK" grepResults.txt;
+then   
+    echo "Passed all tests"
+else
+    TESTSRUN=` cut -d ',' -f 1 grepResults.txt `
 
-FAILURES=` cut -d ',' -f 2 grepResults.txt `
+    FAILURES=` cut -d ',' -f 2 grepResults.txt `
 
-NUMFAILURES="${FAILURES//[^0-9]/}"
-echo $NUMFAILURES
+    NUMFAILURES="${FAILURES//[^0-9]/}"
 
-NUMTESTS="${TESTSRUN//[^0-9]/}"
-echo $NUMTESTS
+    NUMTESTS="${TESTSRUN//[^0-9]/}"
+
+    PASSEDTESTS=`expr $NUMTESTS - $NUMFAILURES`
+    echo $PASSEDTESTS
+
+    SCORE=`expr $PASSEDTESTS / $NUMTESTS`
+    echo "your score is: " $SCORE "%"
+fi
+
+# grep "Tests run" output.txt > grepResults.txt
+
+# TESTSRUN=` cut -d ',' -f 1 grepResults.txt `
+
+# FAILURES=` cut -d ',' -f 2 grepResults.txt `
+
+# NUMFAILURES="${FAILURES//[^0-9]/}"
+# echo $NUMFAILURES
+
+# NUMTESTS="${TESTSRUN//[^0-9]/}"
+# echo $NUMTESTS
+
